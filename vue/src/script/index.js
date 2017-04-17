e79b9606a4b78989dbdd5f3c8b7294c0ef57c9db
高良陽平 <b195.clo5566@gmail.com>
2017-04-17 23:32:10 +0900

    Methodのテストのため、vueインスタンにmethodsを追加。

--- vue/src/script/index.js
+++ vue/src/script/index.js
@@ -1,6 +1,15 @@
 var app = new Vue({
   el: '#app',
   data: {
-    message: 'Hello Vue!'
+    message: 'Hello ',
+    name: ""
+  },
+  methods: {
+    greet: function (event) {
+      this.name = "Vue!";
+    },
+    clrearGreet: function (event) {
+      this.name = "";
+    }
   }
 })
