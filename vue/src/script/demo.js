0444ab18b5afd886af85b7540066561f8e66d673
高良陽平 <b195.clo5566@gmail.com>
2017-04-17 23:32:52 +0900

    Demoの削除

--- vue/src/script/demo.js
+++
@@ -1,2 +0,0 @@
-var testTarget = {};
-testTarget.name = "jasmine-test";
--- vue/test/unit/spec/demoSpec.js
+++
@@ -1,5 +0,0 @@
-describe("demoTest", function() {
-    it("demo getName", function() {
-        expect(testTarget.name).toEqual("jasmine-test");
-    })
-});
